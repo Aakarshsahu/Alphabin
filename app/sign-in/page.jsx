4e2c7887ae8256dfb54cfdcf81b4eee538646de2
Aakarshsahu <aakarshsshu2@gmail.com>
2023-12-20 15:44:59 +0530

    Implement user authentication feature: added sign up, sign in, and logout functionality

---
+++ app/sign-in/page.jsx
@@ -0,0 +1,72 @@
+'use client'
+import { useState } from 'react';
+import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import {auth} from '@/app/firebase/config'
+import { useRouter } from 'next/navigation';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const router = useRouter()
+
+  const handleSignIn = async () => {
+    try {
+        const res = await signInWithEmailAndPassword(email, password);
+        console.log({res});
+        sessionStorage.setItem('user', true)
+        setEmail('');
+        setPassword('');
+        router.push('/')
+    }catch(e){
+        console.error(e)
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      
+      router.push("/sign-up")
+
+    } catch (e) {
+     console.error(e) 
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center text-green-600">
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <h1 className="text-white text-2xl mb-5 text-center">Sign In</h1>
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <button 
+          onClick={handleSignIn}
+          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
+        >
+          Sign In
+        </button>
+        <br /><br />
+        <button 
+          onClick={handleSignUp}
+          className="w-full p-3  rounded text-white  flex justify-end items-center text-green-500"
+        >
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
