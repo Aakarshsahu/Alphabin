068915ca914a1c2d88e642bf016b2a5b25df3bb3
Aakarshsahu <aakarshsshu2@gmail.com>
2023-12-21 18:06:48 +0530

    final touch

--- app/page.jsx
+++ app/page.jsx
@@ -132,8 +132,10 @@ export default function Home() {
           onPageChange={handlePageClick}
           containerClassName={'pagination'}
           activeClassName={'active'}
-          pageClassName={'mx-2 p-2 bg-gray-300 text-gray-700 rounded '} // Style for individual page numbers
+          // pageClassName={'mx-2 p-2 bg-gray-300 text-gray-700 rounded '} // Style for individual page numbers
           breakClassName={'mx-2 p-2 bg-gray-300 text-gray-700 rounded'} // Style for break dots
+          pageClassName={'hidden'} // Hide individual page numbers
+          // breakClassName={'hidden'} // Hide break dots
           previousClassName={'mx-2 p-2 bg-gray-300 text-gray-700 rounded !important'} // Style for previous button
           nextClassName={'mx-2 p-2 bg-gray-300 text-gray-700 rounded !important'} // Style for next button
         />
